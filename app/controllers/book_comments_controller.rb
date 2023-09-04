9a2d51687a1d660ff5938c451925a173f845e851
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-09-04 09:04:44 +0000

    [Initial1-7d]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,6 +3,7 @@ class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     @book_comment = BookComment.new
+    @book_comment.book_id = @book.id
     @comment = current_user.book_comments.new(book_comment_params)
     #上記１行はcomment = BookComment.new(book_comment_params)
     #とcomment.user_id = current_user.idを表す。
